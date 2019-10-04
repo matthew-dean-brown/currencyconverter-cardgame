bf3604fd7cf4a6813a5831766e9c6d7e8b1f66a9
matthew-dean-brown <mattrewbrown@gmail.com>
2019-10-04 16:35:40 +0200

    Update Card1.js

--- src/components/Card1.js
+++ src/components/Card1.js
@@ -18,7 +18,7 @@ class Card1 extends React.Component {
         if (Prompt === "y"){
           window.location.reload()
         } else {
-          window.location.pathname = "task9/"
+          window.location.pathname = "/"
         }
     }
   }
